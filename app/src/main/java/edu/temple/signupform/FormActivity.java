5a0fbd271841a5eff3f3b2cf2ed4b2e7da52aeb8
anthonylittlejohn <tuh40168@temple.edu>
2020-09-20 13:39:29 -0400

    2nd commit - buttons labels added. partial code for button click event added

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -3,6 +3,10 @@ package edu.temple.signupform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -10,5 +14,34 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView name = findViewById(R.id.txtName);
+        TextView email = findViewById(R.id.txtEmail);
+        final TextView password = findViewById(R.id.txtPassword);
+        final TextView confirmPassword = findViewById(R.id.txtConfirm);
+
+        findViewById(R.id.btnSave).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(name.getText().toString().isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(), "Must enter a name", Toast.LENGTH_LONG).show();
+                }
+                else if (email.getText().toString().isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(), "Must enter a name", Toast.LENGTH_LONG).show();
+                }
+                else if (password.getText().toString().isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(), "Must enter a name", Toast.LENGTH_LONG).show();
+                }
+                else if (confirmPassword.getText().toString().isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(), "Must enter a name", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
     }
 }
